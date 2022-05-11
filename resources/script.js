773a4b8e68254bb51158ed25b338e7f07276ea87
Eric Lake <ericallenlake@gmail.com>
2022-05-11 13:01:16 -0600

    Updates scoring
    scoring functionality added with timer

--- resources/script.js
+++ resources/script.js
@@ -2,23 +2,60 @@ let countdown = document.getElementById('countdown');
 let home = document.getElementById('home');
 let q1 = document.getElementById('q1')
 let begin = document.getElementById('begin');
-let choices = document.getElementsByClassName('choice')
+let correctAnswer = document.getElementsByClassName('answer');
+let incorrectAnswer = document.getElementsByClassName('wrong');
+let scoreElement = document.getElementById('score');
+let lastElement = document.getElementById('last');
+let scoreNum = 0;
 
-console.dir(choices[0]);
 
-let addChoices = () => {
-    for (let i = 0; i < choices.length; i++) {
-    choices[i].addEventListener('click',function(){
+home.style.transform = 'translateX(0%)';
+scoreElement.textContent = scoreNum;
+
+
+
+
+// console.dir(choices[0]);
+
+let addCorrect = () => {
+    for (let i = 0; i < correctAnswer.length; i++) {
+    correctAnswer[i].addEventListener('click',function(){
         console.log('clicked an answer');
         console.log(this.parentElement.parentElement);
+        scoreNum += 5;
+        scoreElement.textContent = scoreNum;
+        console.log(scoreNum)
         this.parentElement.parentElement.style.transform = 'translate(-105%)';
         this.parentElement.parentElement.nextElementSibling.style.transform = 'translate(0%)';
+    })
+}
+}
+let addIncorrect = () => {
+    for (let i = 0; i < incorrectAnswer.length; i++) {
+    incorrectAnswer[i].addEventListener('click',function(){
+        console.log('clicked an answer');
+        console.log(this.parentElement.parentElement);
+        this.style.backgroundColor = 'red';
+        timeLeft -= 2;
+        // clearInterval(quizInterval);
 
     })
 }
 }
 
-addChoices();
+let addLast = () => {
+    lastElement.addEventListener('click', function() {
+        // scoreNum += 5;
+        // this.parentElement.parentElement.style.transform = 'translate(-105%)';
+        // this.parentElement.parentElement.nextElementSibling.style.transform = 'translate(0%)';
+        clearInterval(3);
+        console.log('last');
+        console.log(timeLeft);
+        scoreNum+= timeLeft;
+        scoreElement.textContent = scoreNum;
+
+    })
+}
 
 
 begin.addEventListener('click', function(){
@@ -27,20 +64,37 @@ begin.addEventListener('click', function(){
     q1.style.transform = 'translateX(0%)'
 })
 
+
+
 let timeLeft = 60;
 let setTime = () => {
     let timerInterval = setInterval(function(){
         timeLeft--;
-        countdown.textContent = timeLeft
-    }, 1000)
+        countdown.textContent = timeLeft;
+        console.log(timerInterval);
+    }, 1000);
+    
 }
+let quizTimer = () => {
+    timeLeft--;
+    countdown.textContent = timeLeft
+}
+// let quizInterval = () => setInterval(quizTimer, 1000);
+
 
 // setTime()
 
-home.style.transform = 'translateX(0%)'
+
 // q1.style.transform = 'translateX(-100%)'
 
 let startQuiz = () => {
-    setTime()
+    setTime();
+    let countdown = setInterval(function(){quizTimer()}, 1000);
+    console.log(countdown);
     // console.log(event);
 }
+
+addCorrect();
+addIncorrect();
+addLast();
+// clearInterval(quizInterval);
